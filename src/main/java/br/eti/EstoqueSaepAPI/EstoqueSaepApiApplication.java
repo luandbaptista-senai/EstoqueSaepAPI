6661445020822f2a415431b392c2bf393b2d0e9b
Luan <luan.d.baptista@aluno.senai.br>
2026-08-03 11:28:47 -0300

    Criação da versão inicial dos repositórios - Criação do repositório ProdutoRepository - Criação do repositório MovimentoRepository - Criação do repositório CategoriaRepository

--- src/main/java/br/eti/EstoqueSaepAPI/EstoqueSaepApiApplication.java
+++ src/main/java/br/eti/EstoqueSaepAPI/EstoqueSaepApiApplication.java
@@ -1,9 +1,11 @@
 package br.eti.EstoqueSaepAPI;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class EstoqueSaepApiApplication {
 
 	public static void main(String[] args) {
